ba01c6cb791979833c22f68a15b76037f4c61cd8
Evgeny.Terekhin <Evgeny.Terekhin@jetbrains.com>
2026-07-06 11:00:06 +0200

    my_syscall0 implemented
    WIP

--- arch-wasm.h
+++ arch-wasm.h
@@ -5,23 +5,60 @@
 #ifndef NOLIBC_ARCH_WASM_H
 #define NOLIBC_ARCH_WASM_H
 
-#ifndef _NOLIBC_ARCH_ARM_H
-#define _NOLIBC_ARCH_ARM_H
+#ifndef _NOLIBC_ARCH_WASM_H
+#define _NOLIBC_ARCH_WASM_H
 
+#include "linux/unistd.h"
+#include "errno.h"
 #include "compiler.h"
 #include "crt.h"
 
 #define __ARCH_WANT_SYS_OLD_SELECT
 
+#define DEBUGGING_SYSCALLS
+
 int main(int argc, char **argv, char **envp);
 
-long NOT_IMPLEMENTED_SYSCALL() {
-	return 1;
+static __inline__ long NOT_IMPLEMENTED_SYSCALL() {
+    return -ENOSYS; // return syscall is missing
+}
+
+static __inline__ long my_syscall0_handler(long num) {
+    switch (num) {
+        case __NR_setsid:
+        case __NR_sched_yield:
+        case __NR_getuid: // get user id
+        case __NR_geteuid: // effective user id
+#if defined(__NR_geteuid32)
+        case __NR_getuid32:  // effective 32bit user id
+#endif
+        case __NR_gettid: // Impossible to get tid as a number, only info if it is a worker thread or main thread
+        case __NR_getppid:
+        case __NR_getpid:
+#if defined(WASM_STRICT_SYSCALL_IMPLEMENTATION)
+            __builtin_trap(); // "Currenly not supported for wasm"
+#else
+            // TODO: it might be better to fake the values separately
+            return 0;
+#endif
+#if defined(__NR_vfork)
+        case __NR_vfork:
+            __builtin_trap(); // "Currenly not supported for wasm"
+            break;
+#endif
+#if defined(__NR_fork)
+        case __NR_fork:
+            __builtin_trap(); // "Currenly not supported for wasm"
+            break;
+#endif
+        default:
+            __builtin_trap(); //"Not supported for wasm"
+    }
 }
 
 #define my_syscall0(num)                                                      \
 ({                                                                            \
-	NOT_IMPLEMENTED_SYSCALL();                                                \
+    my_syscall0_handler(num);                                                            \
 })
 
 #define my_syscall1(num, arg1)                                                \
@@ -57,12 +94,12 @@ long NOT_IMPLEMENTED_SYSCALL() {
 #ifndef NOLIBC_NO_RUNTIME
 /* startup code */
 void __attribute__((weak, noreturn)) __no_stack_protector _start(void) {
-	// TODO: extract all main arguments properly
-	main(0, 0,0);
+    // TODO: extract all main arguments properly
+    main(0, 0, 0);
 }
 #endif /* NOLIBC_NO_RUNTIME */
 
-#endif /* _NOLIBC_ARCH_ARM_H */
+#endif /* _NOLIBC_ARCH_WASM_H */
 
 
 #endif //NOLIBC_ARCH_WASM_H
